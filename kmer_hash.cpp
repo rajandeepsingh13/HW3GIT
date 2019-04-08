80d368e95a747a8df5563af61aaa4cb856c8aeae
Rajandeep Singh <rajandeepsingh13@gmail.com>
2019-04-07 19:16:16 -0700

    update 2

--- kmer_hash.cpp
+++ kmer_hash.cpp
@@ -109,6 +109,33 @@ bool HashMap::insert(const kmer_pair &kmer) {
   return success;
 }
 
+bool HashMap::find(const pkmer_t &key_kmer, kmer_pair &val_kmer) {
+  uint64_t hash = key_kmer.hash();
+  uint64_t probe = 0;
+  bool success = false;
+  do {
+    uint64_t slot = (hash + probe++) % size();
+    if (slot_used(slot)) {
+      val_kmer = read_slot(slot);
+      if (val_kmer.kmer == key_kmer) {
+        success = true;
+      }
+    }
+  } while (!success && probe < size());
+  return success;
+}
+
+bool HashMap::slot_used(uint64_t slot) {
+  return used[slot] != 0;
+}
+
+void HashMap::write_slot(uint64_t slot, const kmer_pair &kmer) {
+  data[slot] = kmer;
+}
+
+kmer_pair HashMap::read_slot(uint64_t slot) {
+  return data[slot];
+
 int main(int argc, char **argv) {
   upcxx::init();
 
